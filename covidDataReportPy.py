810510ffb8172d492e8e695ebab33da61bf9615d
Gonçalo Fontenele <goncalogfb@poli.ufrj.br>
2020-04-21 15:32:47 -0300

    Comments added to the code. Dataset updated to 21/04/2020

--- covidDataReportPy.py
+++ covidDataReportPy.py
@@ -15,6 +15,11 @@ from tkinter import messagebox
 import requests
 
 '''
+The program is not generalized.
+It only works from the dataset of the links below
+'''
+
+'''
 
 Data came from:
     Brazil: https://covid.saude.gov.br/
@@ -34,35 +39,44 @@ colors = ['blue','orange','green','cyan','olive','fuchsia','lime','darkorange','
 def resetColors():
     colors = ['blue','orange','green','cyan','olive','fuchsia','lime','darkorange','dimgray','darkred','orangered','olivedrab','gold','lightseagreen','darkslategrey','seagreen', 'deepskyblue','dodgerblue','chocolate','goldenrod']
 
+#This will clear the present figure you are using to plot in the moment
+#each time the function is executed it closes a window
 def clr():
     plt.close()
     resetColors()
 
+# download the link of the data you want from humData
 def refreshWorld():
     world.refreshData('dataCOVID.csv')
 
-   
+#Tk inter app 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
+        #creat the master
         self.parent = parent
         self.configureWindow(parent)
         self.msg = 1
 
         
-
-        
+    #The window was designed in order to be placed in the side of the screen,
+    #that´s why it´s so thin
+    # Window configuration: all buttons and labels
     def configureWindow(self, master):
+        #Color of the background
         self.configuredColor = 'white'
+        #Font of the title
         self.configuredFonte = 'Raleway'
         
+        #Title of the root window
         master.title('COVID-19 Analysis')
         master.resizable(0,0)
 
+        #It opens in the superior left side
         master.geometry('200x740+0+0')
         master.configure(bg=self.configuredColor)
     
-        
+        #Creation of two frames that will be placed the other frames
         self.bigFrame1 = tk.Frame(master, bg=self.configuredColor)
         self.bigFrame2 = tk.Frame(master, bg=self.configuredColor)
         
@@ -94,6 +108,11 @@ class MainApplication(tk.Frame):
         
         self.frame4 = tk.Frame(self.bigFrame2, bg=self.configuredColor)
         
+        '''
+        The listbox does not have a scrollbar itself,
+        I decided to let just the scroll of the mouse or the dash movement
+        There is a problem once one of the scrollbar(when placed) it moves the other one
+        '''
         #Frame 4
         self.lbStateListbox = tk.Label(self.frame4, text='Select State to Plot', bg = self.configuredColor)
         self.stateScrollbar = tk.Scrollbar(self.frame4, orient=tk.VERTICAL)
@@ -119,14 +138,14 @@ class MainApplication(tk.Frame):
         self.bigFrame1.pack(padx = 4, pady = 5)
         self.bigFrame2.pack(padx = 4, pady = 5)
         
-        
-
+    
 
         #Date refresh and show updates
         self.dateLabel = tk.Label(self.frame5, text="Last update - US format", bg=self.configuredColor)
         self.dateLabel1 = tk.Label(self.frame5, text="World Data", bg=self.configuredColor)
         self.dateLabel2 = tk.Label(self.frame5, text=world.getDate(), bg=self.configuredColor)
         self.dateLabel3 = tk.Label(self.frame5, text="Brasil Data", bg=self.configuredColor)
+        #The next steps are done in order to get the date in international format not US
         brDate = br.getDate()
         brDate = brDate[5:7]+'/'+brDate[8:10]+'/'+brDate[2:4]
         self.dateLabel4 = tk.Label(self.frame5, text=brDate, bg=self.configuredColor)
@@ -152,7 +171,7 @@ class MainApplication(tk.Frame):
         self.msgButton = tk.Button(master, text="Disable MSG BOX", command=self.desMsg)
         self.msgButton.pack(side=tk.RIGHT, padx = 2, fill=tk.BOTH, expand=1, pady=2)
         
-  
+    #This will plot the country selected on matplotlib figure
     def plotCountryCommand(self):
         index = self.listbox.curselection()
         country = world.data['Country/Region'].drop_duplicates().iloc[index[0]]
@@ -162,6 +181,7 @@ class MainApplication(tk.Frame):
             messagebox.showinfo('Confirmed Cases in '+str(country), str(country)+' has '+str(world.cases)+' confirmed cases of COVID-19.')
         return country
     
+    #This is connected to brazilian data
     def plotStateCommand(self):
         index = self.stateListbox.curselection()
         state = br.data['estado'].drop_duplicates().iloc[index[0]]
@@ -169,6 +189,7 @@ class MainApplication(tk.Frame):
         br.plotState(state)
         return state
     
+    #this will plot the region group by from Brazil
     def plotBarChartRegion(self):
         return br.plotBarChartBy('Region')
     
@@ -179,9 +200,9 @@ class MainApplication(tk.Frame):
         self.msg += -1
         self.msg = abs(self.msg)
         if self.msg == 1:
-            self.msgButton['text'] = 'Disable MSG BOX'
+            self.msgButton['text'] = 'Disable MSG BOX' #This msg is shown with the confirmed cases of each country
         else:
-            self.msgButton['text'] = 'Enable MSG BOX'
+            self.msgButton['text'] = 'Enable MSG BOX' 
 
         return self.msg
     
@@ -189,7 +210,9 @@ class MainApplication(tk.Frame):
         clr()
 
         
-        
+#Top class of the data analysis
+#this one should derivate the other classes
+#like the brazilian one
 class Data:
     def __init__(self, file):
         self.data = self.readDataFrom(file)
@@ -198,7 +221,7 @@ class Data:
         print('Beginning file download with requests of world data from ' + str(url))
 
         self.url = url
-        r = requests.get(self.url)
+        r = requests.get(self.url) #module requests, get data from a website, link on the entry opf function
         
         with open(fileName, 'wb') as f:
             f.write(r.content)
@@ -211,7 +234,7 @@ class Data:
         world = Data('dataCOVID.csv')
 
         
-        
+    #get the last date that was ploted on a graph
     def getDate(self):
         dfList = self.data.columns.values.tolist()
         date = dfList[-1]
@@ -219,27 +242,32 @@ class Data:
         return date
         
     def readDataFrom(self, file):
+        #methods from pandas
         data = pd.read_csv(file, sep=',', encoding='ISO-8859-1')
-        del data['Lat']
+        #Delete the columns that we have no interest
+        del data['Lat'] 
         del data['Long']
         del data['Province/State']
         return data
         
+    #find all data of the country you want, creting a time line basically
     def findDataOf(self, country):
-        regionData = self.data[self.data['Country/Region'].str.contains(country)]
-        del regionData['Country/Region']
+        regionData = self.data[self.data['Country/Region'].str.contains(country)] #Get data which contains the country
+        del regionData['Country/Region'] #No need anymore
         regionData = regionData.sum(axis=0)
-        self.cases = regionData.iloc[-1]
+        self.cases = regionData.iloc[-1] #get the last number of the list
         print ('Number of confirmed cases in ' + str(country) + ' is ' + str(self.cases))
         return regionData
     
+    #Used in order to not repeat colors of the list of colors I Chose
     def color(self, lis):
         color = random.choice(colors)
         colors.remove(color)
         if len(colors) == 0:
-            resetColors()    
+            resetColors()  #If there is no colors to select anymore, it will reset the list
         return color
     
+    #This configure the chart for most of the functions
     def configureChart(self, chart, country, color=''):
         color = self.color(colors)
         plt.title('Confirmed Cases of COVID-19')
@@ -252,7 +280,7 @@ class Data:
         return self.chart
         
     def plotCountry(self, country):
-        dataCountry = self.findDataOf(country)
+        dataCountry = self.findDataOf(country) #use functions already defined
         self.configureChart(dataCountry, country)
         plt.show()
         
@@ -265,12 +293,15 @@ class Data:
         self.configureChart(dataCountry2, country_2, color_2)
         plt.show()
 
+    #Experiment funcion to plot logxlog chart of the confirmed cases by the new cases per day
+    #The results did not go as expected but can be mascared by a function just for analysis purpose
     def logPlot(self, country):
         i=0
         newCases = []
         dataCountry = self.findDataOf(country)
         print ('dataC size is ' + str(dataCountry.size))
         length = dataCountry.size - 1
+        #get the number of daily new cases by iteration
         while i < (length):
             dailyNewCases = dataCountry.iloc[i+1] - dataCountry.iloc[i]
             print (dailyNewCases)
@@ -282,12 +313,13 @@ class Data:
         plt.title('Logaritm scale Cases x New Cases')
         plt.xlabel('New Cases')
         plt.ylabel('Confirmed Cases')
-        plt.yscale('log')
-        plt.xscale('log')
+        plt.yscale('log') #changes the scale to log on y axis
+        plt.xscale('log') #the same for x
         plt.grid(True)
         plt.plot(dataCountry, newCases)
         plt.show()
 
+#This is a derivated class from data
 class Brasil(Data):        
     def readDataFrom(self, file):
         data = pd.read_csv(file, sep=';', encoding='ISO-8859-1')
@@ -299,6 +331,9 @@ class Brasil(Data):
         print ('In international Format')
         return date
     
+    #Gets a group of the states or regions
+    #In the future it may be able to get states per region
+    #not done in this version
     def groupBy(self, by_Region_or_State):
         by = by_Region_or_State
         dic = {'Region':'regiao','State':'estado'}
@@ -306,12 +341,13 @@ class Brasil(Data):
         a.columns = ['Confirmed cases','Deaths']
         return a
     
+    #If you want to see the number of deaths separated from cases, try in_same_chart = False, or actually anything else other than True
     def plotBarChartBy(self, by_Region_or_State, in_same_chart=True):
         if in_same_chart == True:
             sp = False
         else:
             sp = True
-        by = by_Region_or_State
+        by = by_Region_or_State #defines if the bar chart is going to be ploted by region or brazilian state
         df = self.groupBy(by)
         color_1 = self.color(colors)
         color_2 = self.color(colors)
@@ -323,28 +359,30 @@ class Brasil(Data):
         
     def findDataOf(self, state, region=''):
         regionData = self.data[self.data['estado'].str.contains(state)]
-        del regionData['obitosNovos']
-        del regionData['casosNovos']
-        del regionData['estado']
-        if region == '':
+        del regionData['obitosNovos'] #Not needed
+        del regionData['casosNovos'] #Not needed
+        del regionData['estado'] #Not needed
+        if region == '': #See if you want some region specific
             del regionData['regiao']
-        cases = regionData['casosAcumulados'].iloc[-1]
+        cases = regionData['casosAcumulados'].iloc[-1] #Gets the last number of cumulated cases
         print ('Number of confirmed cases in ' + str(state) + ' is ' + str(cases))
         return regionData
 
-    def configureChartForCurve(self, chart, state, color=''):
-        fig, (ax1, ax2) = plt.subplots(1,2, figsize=(8,3), gridspec_kw={'wspace':.2})
+    def configureChartForCurve(self, chart, state):
+        fig, (ax1, ax2) = plt.subplots(1,2, figsize=(8,3), gridspec_kw={'wspace':.2}) #Create a figure with two subplots
         color = self.color(colors)
-        dates = []
+        dates = [] #Create an empty list
+        #Fulfill the list with all the dates
         for item in chart['data']:
             item = str(item)
             dates.append(item)
         ax1.plot(dates,chart['casosAcumulados'])
         ax2.plot(dates,chart['obitosAcumulados'])
         
+        #Title of the figure
         fig.suptitle('COVID-19 Analysis - ' + str(state), fontsize=16)
         
-        
+        #Configure the first chart
         ax1.set_title('Cases of COVID-19 in '+ str(state))
         ax1.set_xlabel('Date')
         ax1.set_ylabel('Confirmed Cases')
@@ -352,6 +390,7 @@ class Brasil(Data):
         #ax1.legend(loc='best')
         ax1.grid(True)
         
+        #Configure the second chart
         ax2.set_title('Deaths of COVID-19 in '+ str(state))
         ax2.set_xlabel('Date')
         ax2.set_ylabel('Deaths')
@@ -359,6 +398,7 @@ class Brasil(Data):
         #ax2.legend(loc='best')
         ax2.grid(True)
         
+        #This is used due to the fact there are a lot of dates, so we plot just nbins of then
         locator=ticker.MaxNLocator(prune='both', nbins=5)
         ax1.xaxis.set_major_locator(locator)
         ax2.xaxis.set_major_locator(locator)
@@ -373,13 +413,15 @@ class Brasil(Data):
         self.configureChartForCurve(dataState, state).show()
         
 
-
+#App mainloop
 if __name__ == "__main__":
     root = tk.Tk()
     #Creation of the dataframes of WorldData and BrasilData
     #It needs to be created here in order to the script work
     world = Data('dataCOVID.csv')
     br = Brasil('dataBrasil.csv')
+    #The root window will always be toplevel
+    #If you dont want it, just comment the next line
     MainApplication(root).pack(side="top", fill="both", expand=True)
     root.call('wm', 'attributes', '.', '-topmost', '1')
     root.mainloop()
